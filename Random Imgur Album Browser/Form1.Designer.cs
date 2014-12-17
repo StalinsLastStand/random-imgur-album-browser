c78c7fb8ef5a6d0ba09ae1afbb71c16fb1e19f7e
Ben Bristow <me@benbristow.co.uk>
2014-12-17 21:33:19 +0000

    Initial commit

---
+++ Random Imgur Album Browser/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Random_Imgur_Album_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.txt_URL = new System.Windows.Forms.TextBox();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbl_CurrentID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(696, 469);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // txt_URL
+            // 
+            this.txt_URL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txt_URL.Location = new System.Drawing.Point(0, 0);
+            this.txt_URL.Name = "txt_URL";
+            this.txt_URL.ReadOnly = true;
+            this.txt_URL.Size = new System.Drawing.Size(696, 20);
+            this.txt_URL.TabIndex = 1;
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_Next.Location = new System.Drawing.Point(0, 446);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(696, 23);
+            this.btn_Next.TabIndex = 2;
+            this.btn_Next.Text = "Next!";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 423);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(696, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // lbl_CurrentID
+            // 
+            this.lbl_CurrentID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_CurrentID.AutoSize = true;
+            this.lbl_CurrentID.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl_CurrentID.Location = new System.Drawing.Point(601, 430);
+            this.lbl_CurrentID.Name = "lbl_CurrentID";
+            this.lbl_CurrentID.Size = new System.Drawing.Size(86, 13);
+            this.lbl_CurrentID.TabIndex = 4;
+            this.lbl_CurrentID.Text = "Trying ID: 00000";
+            this.lbl_CurrentID.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 469);
+            this.Controls.Add(this.lbl_CurrentID);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.txt_URL);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "Form1";
+            this.Text = "imgur Random Albums";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox txt_URL;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbl_CurrentID;
+    }
+}
+
---
+++ Random Imgur Album Browser/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Random_Imgur_Album_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+           findRandomURL();
+        }
+
+
+        private async Task findRandomURL()
+        {
+            lbl_CurrentID.Text = "";
+            lbl_CurrentID.Visible = true;
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+
+            bool found = false;
+
+            while (!found)
+            {
+               string id = await generateRandomString();
+               lbl_CurrentID.Text = string.Format("Trying ID: {0}", id);
+
+               if (await checkIfValid(id))
+               {
+                    string url = string.Format("http://imgur.com/a/{0}/", id);
+
+                    webBrowser.Navigate(url);
+                    txt_URL.Text = url;
+                    progressBar1.Value = 0;
+                    progressBar1.Visible = false;
+                    lbl_CurrentID.Text = null;
+
+                    found = true;
+
+
+               }
+               else
+               {
+                   progressBar1.Value += 1;
+               }                               
+            }
+        }
+
+        private async Task<string> generateRandomString()
+        {
+            Random rnd = new Random();
+            string output = "";
+
+            while (output.Length != 5)
+            {
+                int r = rnd.Next(1, 4);
+
+                switch (r)
+                {
+                    case 1:
+                        //Upper letter
+                        char u = (char)rnd.Next(65, 91);
+                        output += u;
+                        break;
+                    case 2:
+                        //Lowercase Letter
+                        char l = (char)rnd.Next(97, 123);
+                        output += l;
+                        break;
+                    case 3:
+                        //Number
+                        output += rnd.Next(1, 10).ToString();
+                        break;
+                }
+            }
+
+            return output;
+        }
+
+        private async Task<Boolean> checkIfValid(string id)
+        {
+            WebClient wc = new WebClient();
+            Uri u = new Uri(string.Format("https://imgur.com/a/{0}/", id));
+
+            try
+            {
+                string html = await wc.DownloadStringTaskAsync(u);
+                if (html.Contains("This album is empty"))
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
